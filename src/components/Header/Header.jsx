1637dabbcd06598ee0f0b811a00048b38c0068a1
Zaheen Rakib <rakibzaheen@gmail.com>
2024-03-25 23:59:16 +0600

    Create Banner Section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -19,15 +19,16 @@ const Header = () => {
                         {links}
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">daisyUI</a>
+                <a className="font-bold text-2xl">Book Vibe</a>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
                     {links}
                 </ul>
             </div>
-            <div className="navbar-end">
-                <a className="btn">Button</a>
+            <div className="navbar-end flex gap-5">
+                <a className="btn btn-success text-white">Sign In</a>
+                <a className="btn btn-accent text-white">Sign Up</a>
             </div>
         </div>
     );
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,17 @@
-
+import MyImaged from '../images/cover.jpg'
 const Home = () => {
     return (
-        <div>
-            <h1 className="text-4xl">This Is Home Page</h1>
-        </div>
+        <>
+            <div className="hero min-h-screen bg-base-200">
+                <div className="lg:flex items-center justify-around flex-col lg:flex-row-reverse">
+                    <img src={MyImaged} className="max-w-sm rounded-lg shadow-2xl" />
+                    <div>
+                        <h1 className="text-6xl w-3/4 font-bold">Books to freshen up your bookshelf</h1>
+                        <button className="btn btn-success">Get Started</button>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
